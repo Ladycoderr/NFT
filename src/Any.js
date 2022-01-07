3f7484acfe70006b097d9359404098017ca6ddc0
Ladycoderr <shrutimishra2884@gmail.com>
2022-01-07 11:25:35 +0530

    nft

---
+++ src/Any.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Any extends Component {
+    render() {
+        return (
+            <div>
+                hrep
+            </div>
+        )
+    }
+}
